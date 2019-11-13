e4a441678d9d3511339c35bf92a542bc61390baf
hans aparicio <hans.moreno@drillinginfo.com>
2019-11-12 23:18:54 -0300

    -Changed icon buttons -Changed translations

--- Java/Demo/app/src/main/java/io/chirp/sdkdemoapp/MainActivity.java
+++ Java/Demo/app/src/main/java/io/chirp/sdkdemoapp/MainActivity.java
@@ -13,8 +13,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.Random;
-
 import io.chirp.chirpsdk.ChirpSDK;
 import io.chirp.chirpsdk.interfaces.ChirpEventListener;
 import io.chirp.chirpsdk.models.ChirpSDKState;
@@ -30,16 +28,13 @@ public class MainActivity extends AppCompatActivity {
 
     TextView status;
     TextView lastChirp;
-    TextView versionView;
-
     Button startStopSdkBtn;
-    Button startStopSendingBtn;
 
     Boolean startStopSdkBtnPressed = false;
 
-    String CHIRP_APP_KEY = "YOUR_APP_KEY";
-    String CHIRP_APP_SECRET = "YOUR_APP_SECRET";
-    String CHIRP_APP_CONFIG = "YOUR_APP_CONFIG";
+    String CHIRP_APP_KEY = "F819ACbbcFf6374bBa9D8CeaB";
+    String CHIRP_APP_SECRET = "86D7EEFa957A708DE150DC3C78d0852daa8aef64de1D8906C5";
+    String CHIRP_APP_CONFIG = "DvDw6G2pGlIDMkWFTs9LywIocs9FeDk7LGRx53RMSRYVo+NRalInZn4/8LjNebHoqkf1iqYWt9fdeKm7XZPe07uCXnS1dxfNTdj2VHnjVs+WLG8GIe5RFZITgnqSCLzqx6KksxmDFHk3XK7pn8aYRseHM8X5aa7usnMyu1Pjc21tsfUrjtT/a7H9A1LtnOXkDegu1PCbRoK/HDad1BKjH04hiEUUsTeyjafw0LuUNmHOAvPbMQOmVBtBBYhbgQUh/7rtCQSkCw4+03xZvBmThw+Lb3GyurAKz1eCJnepuiKlV3OfZcJ+INmB6w77yuSyDAsiNLnfty5yhXuNEwcfxJoEIrJtstiCkeew3oRzmyLh3RX2MStBP4hTFMZaEAbV6r90OOsty/d3mmSHRoa0BEN/ouLMC4dWRVU1tnuVAmkTfUzNmHzIRa6W9SdpL3if+nhb7cEeXxQtOw3uJbTx/xlp5wNhB5XGbzEWkgL3Q4VFGACj3RyBJILwCPVqdKWB+k+aLbGOU5LLmdJiigEK7OLXEYCHkJndfMe/TjoD/YEBgWMT6NsAZsO743OobGDa4KYtgAwu0itOUET7jsfbNzSASiyfNyMEHvqvvXfPdnFO/xnx3Tud1mRmS9vd6GR0b+j8WoZxy67dxvPNQCXN7pC9yqxcrLnVhZdksCQ1oFLME0u7FC1X/YmvLTEKf4pzJNmHp60wSLkNMbvne5egnDO8f3MK5RAI/cu1647nRxO1vCvzurjBeMO2sd2wg0WNjuIa+0UWH5N8DUA8Ougf5vNjL1xRTrvoPOEJz3V2FtZ6A6uSTJGvZ9aifXkqLQ/8lv/bRG7Dsydwl8few3OgeM61NAjJp17F6qacQsY2b/COw+iT6BtoDUxlIvhFaAfsMk9mturfaSgMj61sEbi9UCD7RygX3whhfPa/tG6y/fnrJ/BWhytk6p5SSQMvavvNg1ZTr6G5Ux/Wtp8TslVKBtYDibvj1YcvA69J5fWoL3Jql/UoElJOirunc1fFIGNe9HPx4kr/BBGyoip67TxLbVXA0udH6B0tkxeX/ckqM104W/IWdS+j4OR4HEbUqAhlMUBoqOlRojAb1aPn47BpZGp9oFagvJT2peguoGxrAgVjqUuVWu4ZLKAtnbftH7kxZFYA+3u9C3gK1oWzvbTVRU/ND3lsyWlygePzyAZBjSReRM6tU4EjbyGSqGLjiKYaElQQnx4bUddi+6mekfWfi0JSlwBXb2n8deAKtqQFffW7r7lmrMU5UuPuXWaz9zaXJj69gFpe7FtlepAZnU6lh5b6XbjZpGaj36ZsW27jfPvKOGPeMsvwghcW7mvs/UtqOcDJA2WaSoVL3zV1gSf0+gcOVd3V9/T5MPXMxbZ0zsAyNnEpObx8Lsbx/PSNjcj6gjdngAi0M3WP7zxUrG25sc4HC/ZTMRn2OOLm5zLwJpE4ErF3ApmvQ8ydme2LyIOz6Pkd5qUi3NWr0OY5UxMU87PZHg+XDZshDCso87V75LBiuPET9HVFJVboneqScTfsoTNW94DUoDhxDX9dPiaUt7+nc/H/mQxjpRYGl7bC+Edi52jkRs+wbHdY7QfF8HCIv8BPytuFbOC/DBl5iSyKZ1zj9HfFut7SoJ8X0r3YerGtxBv/dLcsDPZ3gf4mIO3bFh56KHoXtRS52snoepXfjM4wcUVNssZhN595ST98aTYJFfLwtRe43Hob09v+CmUC7m0+lAHCNxpj9or9uBsmUJiUBOaB+0lb0aJYbj1NfBSpjdq0ymQk007vdXdeA607ch3ndeMAuS7I4818UE6zfdgtL+RSijfeEND3xI2gYtDLPrRuDrufXtZpoqCmqvLZX+rqypAS5BwHRLSV+/Q5q5nxmCBqIQeZ4gKObwi1UbDx2bQpNxGyt6DUSauH5NgtjtHQcqyoyvbZBrZrWExxhfK20uUKnX7qFo2Sd2KFBcei9lKPi64fsub3Lgap89FzOi6b2RV7TLdKkOmu9NytkQsiA4jIAzJmRhVd/IRECPoZdaozi0tevFV5hz05937BobCX2eyRPZzPQAKPQuBODRC2SxWI6mgwj+cG7KD2H8DaAyf6L47RTW7ertEYbYoAicj8e75n1KwA2mqndIw56wOi2Br3JtDiRKY0WXOBGo8nI/Rs6U1o2rlMINzGc6nKMVPOhwPU9aooi/muLkDuuqoWw0dlCBRnTE9Y6J/8WPky1XthL4fch1w5VaQ+2wbiWhjquH5fDkHIRgCxSKPJW4oKD96ZFWPl+cAC9z1/gk51AtyqZbkM0eZxfQdBcWCDC5HvJdq/IrFtF/XU5Cd5obxqPjoUnQarBQj6G6N+yrcqggYm5+4bEZpkcacxSJ73sroBKrg+FkuWQA3Mh00BiV0Eyb2xjSWDsavlj9PkYOm140F/Kko0GoBkrTrc0AEouVnbcW88IoMRnoXa0anxjTvY7hqT7Jv/vNYdxdw6bsqSH9cPxcG0xxNmmZyESzhLXmLQLMwrdfsMMoypHVT5HKZ4R/sy85brxLWUbIXT/6F7aoD3hFDvRsx1D8h25U3JVbrzxuhP6CYzC7UkNzCBR5uGdbrVtkOVJHX0NJ5mEs0=";
 
     String TAG = "ChirpSDKDemoApp";
 
@@ -49,35 +44,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         parentLayout = findViewById(android.R.id.content);
-
         status = (TextView) findViewById(R.id.stateValue);
         lastChirp = (TextView) findViewById(R.id.lastChirp);
-        versionView = (TextView) findViewById(R.id.versionView);
         startStopSdkBtn = (Button) findViewById(R.id.startStopSdkBtn);
-        startStopSendingBtn = (Button) findViewById(R.id.startStopSengingBtn);
 
-        startStopSendingBtn.setAlpha(.4f);
-        startStopSendingBtn.setClickable(false);
         startStopSdkBtn.setAlpha(.4f);
         startStopSdkBtn.setClickable(false);
 
         context = this;
 
-        if (CHIRP_APP_KEY.equals("") || CHIRP_APP_SECRET.equals("")) {
-            Log.e(TAG, "CHIRP_APP_KEY or CHIRP_APP_SECRET is not set. " +
-                    "Please update with your CHIRP_APP_KEY/CHIRP_APP_SECRET from developers.chirp.io");
-            return;
-        }
-
-        /**
-         * Key and secret initialisation
-         */
         chirpSdk = new ChirpSDK(this, CHIRP_APP_KEY, CHIRP_APP_SECRET);
-
-        Log.v(TAG, "ChirpSDK Version: " + chirpSdk.getVersion());
-        versionView.setText(chirpSdk.getVersion());
 
         ChirpError setConfigError = chirpSdk.setConfig(CHIRP_APP_CONFIG);
         if (setConfigError.getCode() > 0) {
@@ -86,9 +63,6 @@ public class MainActivity extends AppCompatActivity {
             startStopSdkBtn.setAlpha(1f);
             startStopSdkBtn.setClickable(true);
         }
-        String versionDisplay = chirpSdk.getVersion() + "\n" +
-                chirpSdk.getProtocolName() + " v" + chirpSdk.getProtocolVersion();
-        versionView.setText(versionDisplay);
         chirpSdk.setListener(chirpEventListener);
     }
 
@@ -154,15 +128,15 @@ public class MainActivity extends AppCompatActivity {
              */
             Log.v(TAG, "ChirpSDKCallback: onStateChanged " + oldState + " -> " + newState);
             if (newState == ChirpSDKState.CHIRP_SDK_STATE_NOT_CREATED.getCode()) {
-                updateStatus("NotCreated");
+                updateStatus("Bo creado");
             } else if (newState == ChirpSDKState.CHIRP_SDK_STATE_STOPPED.getCode()) {
-                updateStatus("Stopped");
+                updateStatus("Detenido");
             } else if (newState == ChirpSDKState.CHIRP_SDK_STATE_RUNNING.getCode()) {
-                updateStatus("Running");
+                updateStatus("Escuchando");
             } else if (newState == ChirpSDKState.CHIRP_SDK_STATE_SENDING.getCode()) {
-                updateStatus("Sending");
+                updateStatus("Enviando");
             } else if (newState == ChirpSDKState.CHIRP_SDK_STATE_RECEIVING.getCode()) {
-                updateStatus("Receiving");
+                updateStatus("Recibiendo");
             } else {
                 updateStatus(newState + "");
             }
@@ -196,7 +170,7 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.RECORD_AUDIO}, RESULT_REQUEST_RECORD_AUDIO);
         }
         else {
-            if (startStopSdkBtnPressed) startSdk();
+            if (startStopSdkBtnPressed) startListening();
         }
     }
 
@@ -205,7 +179,7 @@ public class MainActivity extends AppCompatActivity {
         switch (requestCode) {
             case RESULT_REQUEST_RECORD_AUDIO: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (startStopSdkBtnPressed) stopSdk();
+                    if (startStopSdkBtnPressed) stopListening();
                 }
                 return;
             }
@@ -232,7 +206,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onStop() {
         super.onStop();
-        stopSdk();
+        stopListening();
     }
 
     public void updateStatus(final String newStatus) {
@@ -252,58 +226,31 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void stopSdk() {
+    public void stopListening() {
         ChirpError error = chirpSdk.stop();
         if (error.getCode() > 0) {
             Log.e(TAG, error.getMessage());
             return;
         }
-        startStopSendingBtn.setAlpha(.4f);
-        startStopSendingBtn.setClickable(false);
-        startStopSdkBtn.setText("Start Sdk");
+
+        startStopSdkBtn.setText("Iniciar Escucha");
     }
 
-    public void startSdk() {
+    public void startListening() {
         ChirpError error = chirpSdk.start();
         if (error.getCode() > 0) {
             Log.e(TAG, error.getMessage());
             return;
         }
-        startStopSendingBtn.setAlpha(1f);
-        startStopSendingBtn.setClickable(true);
-        startStopSdkBtn.setText("Stop Sdk");
+        startStopSdkBtn.setText("Detener");
     }
 
     public void startStopSdk(View view) {
-        /**
-         * Start or stop the SDK.
-         * Audio is only processed when the SDK is running.
-         */
         startStopSdkBtnPressed = true;
         if (chirpSdk.getState() == ChirpSDKState.CHIRP_SDK_STATE_STOPPED) {
-            startSdk();
+            startListening();
         } else {
-            stopSdk();
-        }
-    }
-
-    public void sendPayload(View view) {
-    	/**
-         * A payload is a byte array dynamic size with a maximum size defined by the config string.
-         *
-         * Generate a random payload, and send it.
-         */
-    	long maxPayloadLength = chirpSdk.maxPayloadLength();
-    	long size = (long) new Random().nextInt((int) maxPayloadLength) + 1;
-        byte[] payload = chirpSdk.randomPayload((byte) size);
-        long maxSize = chirpSdk.maxPayloadLength();
-        if (maxSize < payload.length) {
-            Log.e(TAG, "Invalid Payload");
-            return;
-        }
-        ChirpError error = chirpSdk.send(payload);
-        if (error.getCode() > 0) {
-            Log.e(TAG, error.getMessage());
+            stopListening();
         }
     }
 
